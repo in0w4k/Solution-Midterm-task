a86be60557f992f990184c931f9c2e4b0800e2d9
in0w4k <in0w4.k@gmail.com>
2025-03-16 14:12:57 +0500

    Create Raff(Coffee)

---
+++ Coffee_Shop_Simulator/Coffee/Raff.java
@@ -0,0 +1,13 @@
+package Coffee_Shop_Simulator.Coffee;
+
+public class Raff implements Coffee {
+    @Override
+    public int getCost() {
+        return 1400;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Raff";
+    }
+}
